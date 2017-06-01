728d3142eebafb66e3ce7d79d238d5f04908d537
karinasilva <karinadasilva276@gmail.com>
2017-06-01 15:43:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert, Image } from 'react-native';
 import { Constants } from 'expo';
+import { StackNavigator } from 'react-navigation'
+class HistóriaSonhosComEt extends Component {
+  render(){
+    return(
+      <View>
+      Sonhos Místicos nos faz duvidar de toda "realidade" em que acreditamos. EU tenho sonhado muito com Et's,em alguns me peguei com muito medo,em outros eu gostei porque tive um contato harmonico... porém desconhecido de tudo que eu acreditava. ão tenho medo porque tenho um pouco de conhecimento e a noção de que não existi vida só no planeta terra.
+      <Text>SonhosComEt</Text>
+      </View>
+   );
+  }
+}
 
-export default class App extends Component {
+class TelaPrincipal extends Component {
   _handleButtonPress = () => {
     Alert.alert(
-      'ir!',
+      'História!',
       'ler a história!',
     );
   };
 
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-      <Text style = {styles.destaque}> tela principal </Text>
+      <Image style={styles.container} source={{uri: "https://68.media.tumblr.com/34c56c0fa7f6ffbd90f0a00748899342/tumblr_n1trgggcIT1ttfj7uo1_500.jpg"}}>
+      <Text style = {styles.destaque}> Tela Principal </Text>
      
         <Text style={styles.paragraph}>
           Histórias em quadrinhos
         
         </Text>
+        
+        
            <Button
-            title="História 1:
-            sonhos com Et's"
-            onPress={this._handleButtonPress}
+            title="História 1: sonhos com Et's"
+            onPress={() => navigate('HistoriaEt')}
             
           />
           
@@ -30,6 +43,7 @@ export default class App extends Component {
           <Button
             title="História 2: viagens astrais"
             onPress={this._handleButtonPress}
+          
           />
     
         
@@ -38,14 +52,21 @@ export default class App extends Component {
             onPress={this._handleButtonPress}
           
           />
-      </View>
+      </Image>
     );
   }
 }
 
+const HistóriasEmQuadrinhos = StackNavigator({
+  Main: {screen: TelaPrincipal},
+  HistoriaEt: {screen: HistóriaSonhosComEt},
+});
+
+export default HistóriasEmQuadrinhos
+
 const styles = StyleSheet.create({
   destaque:{
-    fontSize: 35
+    fontSize: 30
     
     
   },
@@ -54,11 +75,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#999966',
+    backgroundColor: 'pink',
   },
   paragraph: {
     margin: 35,
-    fontSize: 35,
+    fontSize: 25,
     fontWeight: 'bold',
     textAlign: 'center',
     color: ' #0066ff',
